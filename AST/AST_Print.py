16c7b4974dd140a2d4ce6db0b257e9af10d45ec1
Nadia <nadia.gonzalez@estudiantes.matcom.uh.cu>
2020-11-29 16:57:53 -0500

    AST Print changed

--- AST/AST_Print.py
+++ AST/AST_Print.py
@@ -28,15 +28,57 @@ class FormatVisitor(object):
     
     @visitor.when(AttrDeclarationNode)
     def visit(self, node, tabs=0):
-        ans = '\t' * tabs + f'\\__AttrDeclarationNode: {node.id} : {node.type} <- {node.expr}'
+        val = self.visit(node.val)
+        ans = '\t' * tabs + f'\\__AttrDeclarationNode: {node.id} : {node.type} = {val}'
         return f'{ans}'
-    
+
+    @visitor.when(ConditionalNode)
+    def visit(self, node, tabs=0):
+        if_expr = self.visit(node.if_expr)
+        then_expr = self.visit(node.then_expr)
+        else_expr = self.visit(node.else_expr)
+        ans =  '\t' * tabs + f'\\__ConditionalNode: if {if_expr} then {then_expr} else {else_expr}'
+        return f'{ans}'
+
+    @visitor.when(LoopNode)
+    def visit(self, node, tabs=0):
+        ans = '\t' * tabs + f'\\__LoopNode: while {node.condition} -> <body>'
+        body = self.visit(node.body)
+        return f'{ans}\n{body}'
+
+    @visitor.when(BlockNode)
+    def visit(self, node, tabs=0):
+        ans = '\t' * tabs + '\\__BlockNode: {'
+        ans += '\n'.join(self.visit(child, tabs + 1) for child in node.expr_list)
+        ans += ' }'
+        return f'{ans}'
+        
+    @visitor.when(LetNode)
+    def visit(self, node, tabs=0):
+        params = ', '.join(self.visit(child) for child in node.var_list)
+        body = self.visit(node.body)
+        ans = '\t' * tabs + f'\\__LetNode: let {params} in {body}'
+        return f'{ans}'
+
+    @visitor.when(CaseNode)
+    def visit(self, node, tabs={0}):
+        expr = self.visit(node.expr)
+        branches = '\n'.join(self.visit(branch, tabs + 1) for branch in node.branch_list)
+        ans = '\t' * tabs + f'\\__CaseNode: case {expr} of {branches} esac'
+        return f'{ans}'
+
     @visitor.when(AssignNode)
     def visit(self, node, tabs=0):
         ans = '\t' * tabs + f'\\__AssignNode: let {node.id} = <expr>'
         expr = self.visit(node.expr, tabs + 1)
         return f'{ans}\n{expr}'
-    
+
+    @visitor.when(CallNode)
+    def visit(self, node, tabs=0):
+        obj = self.visit(node.obj, tabs + 1)
+        ans = '\t' * tabs + f'\\__CallNode: <obj>.{node.id}:{node.ancestor}(<expr>, ..., <expr>)'
+        args = '\n'.join(self.visit(arg, tabs + 1) for arg in node.args)
+        return f'{ans}\n{obj}\n{args}'
 
     @visitor.when(BinaryNode)
     def visit(self, node, tabs=0):
@@ -48,13 +90,6 @@ class FormatVisitor(object):
     @visitor.when(AtomicNode)
     def visit(self, node, tabs=0):
         return '\t' * tabs + f'\\__ {node.__class__.__name__}: {node.lex}'
-    
-    @visitor.when(CallNode)
-    def visit(self, node, tabs=0):
-        obj = self.visit(node.obj, tabs + 1)
-        ans = '\t' * tabs + f'\\__CallNode: <obj>.{node.id}(<expr>, ..., <expr>)'
-        args = '\n'.join(self.visit(arg, tabs + 1) for arg in node.args)
-        return f'{ans}\n{obj}\n{args}'
     
     @visitor.when(InstantiateNode)
     def visit(self, node, tabs=0):

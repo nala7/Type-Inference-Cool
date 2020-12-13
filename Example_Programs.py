afb0c264f22b7cd8143f7a6bedb6b1a3e08ccbc3
Nadia <nadia.gonzalez@estudiantes.matcom.uh.cu>
2020-12-12 19:37:29 -0500

    ejemplos

--- Example_Programs.py
+++ Example_Programs.py
@@ -453,9 +453,47 @@ class Main inherits IO {
 
 text7 = '''
     class A {
-        a : AUTO_TYPE <- 7 + 6 ;
         f ( ) : Void {
             ( ( new A ) . type_name ( ) ) . length ( ) + 8
         } ;
     } ;
     '''
+
+text8 = '''
+class A inherits IO {
+	f ( x : Int , y : Int ) : Int { x + y } ;
+	g ( x : Int ) : Int { x + x } ;
+} ;
+class B inherits A {
+	f ( a : Int , b : Int ) : Int { a - b } ;
+} ;
+class C inherits B {
+	ident ( m : Int ) : Int { m } ;
+	f ( m : Int , n : Int ) : Int { m * n } ;
+} ;
+class D inherits B { 
+	ident ( v : String ) : IO { ( new IO ) . out_string ( v ) } ;
+	f ( v : Int , w : Int ) : Int { v / w } ;
+	g ( v : Int ) : Int { v + v + v } ;
+
+	back ( s : String ) : B { {
+		out_string ( s ) ;
+		self ; 
+	} } ;
+} ;
+
+class Main inherits IO {
+	main ( ) : IO { out_string ( " Hello World! " ) } ;
+
+	test : B <- ( ( new D ) . back ( " Hello " ) ) . back ( " World! " ) ;
+} ;
+
+'''
+
+text9 = '''
+class Main inherits IO {
+	main ( ) : IO { out_string ( " Hello World! " ) } ;
+
+	test : B <- ( ( new D ) . back ( " Hello " ) ) . back ( " World! " ) ;
+} ;
+'''

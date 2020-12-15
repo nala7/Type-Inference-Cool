310a6b50ead7584171b08d6b80fa6f01c019d605
Nadia <nadia.gonzalez@estudiantes.matcom.uh.cu>
2020-12-15 14:56:36 -0500

    main

--- Main.py
+++ Main.py
@@ -16,38 +16,70 @@ from SemanticChecker.Type_Builder import TypeBuilder
 from SemanticChecker.Type_Checker import TypeChecker
 from SemanticChecker.Type_Inferer import TypeInferer
 
+from altair.vegalite.v4.schema.core import Parse
+from cmp.semantic import Scope
+import AST.AST_Print as print_ast
+import streamlit as st 
+import pickle
+import os
+
+from Example_Programs import *
+from Tokenizer import *
+from Parser.Parser_LR1 import LR1Parser
+from Grammar import *
+from cmp.evaluation import evaluate_reverse_parse
+from SemanticChecker.Scope_Print import Scope_Print
+from SemanticChecker.Type_Collector import TypeCollector
+from SemanticChecker.Type_Builder import TypeBuilder
+from SemanticChecker.Type_Checker import TypeChecker
+from SemanticChecker.Type_Inferer import TypeInferer
+from Serializer import Serializer
+
+st.sidebar.header("About")
+st.sidebar.subheader("Segundo Proyecto de Compilacion: Cool Type Inferer")
+st.sidebar.text("Nadia González Fernández")
+st.sidebar.text("Jose Alejandro Labourdette-Lartigue Soto")
+st.sidebar.text("Grupo: C-312")
+
+
+""" # Cool Intrepreter """
+
+data = st.text_area("Enter code", "", 500)
+run_analysis = st.button("Run", "")
+
 def run_pipeline(G, text):
-   print('=================== TEXT ======================')
-   print('================== TOKENS =====================')
+   st.write('=================== TEXT ======================')
+   st.write(text)
+   st.write('================== TOKENS =====================')
    tokens = tokenize_text(text)
-   print('=================== PARSE =====================')
+   st.write('=================== PARSE =====================')
    parser = Serializer.load(os.getcwd() + '/parser')
 
    parse, operations = parser([t.token_type for t in tokens], get_shift_reduce=True)
 
-   print('==================== AST ======================')
+   st.write('==================== AST ======================')
    ast = evaluate_reverse_parse(parse, operations, tokens)
    formatter = print_ast.FormatVisitor()
    tree = formatter.visit(ast)
-   print(tree)
-   print('============== COLLECTING TYPES ===============')
+   st.write(tree)
+   st.write('============== COLLECTING TYPES ===============')
    errors = []
    collector = TypeCollector(errors)
    collector.visit(ast)
    context = collector.context
-   print('Errors:', errors)
-   print('Context:')
-   print(context)
-   print('=============== BUILDING TYPES ================')
+   st.write('Errors:', errors)
+   st.write('Context:')
+   st.write(context)
+   st.write('=============== BUILDING TYPES ================')
    builder = TypeBuilder(context, errors)
    builder.visit(ast)
-   print('Errors: [')
+   st.write('Errors: [')
    for error in errors:
       print('\t', error)
-   print(']')
-   print('Context:')
-   print(context)
-   print('=============== CHECKING TYPES ================')
+   st.write(']')
+   st.write('Context:')
+   st.write(context)
+   st.write('=============== CHECKING TYPES ================')
    old_errors = errors.copy()
    checker = TypeChecker(context, old_errors)
    scope, infered_types, auto_types = checker.visit(ast)
@@ -60,16 +92,19 @@ def run_pipeline(G, text):
          errors = old_errors
          break
 
-   print('Scope:')
+   st.write('Scope:')
    scope_tree = Scope_Print().visit(scope)
-   print(scope_tree)
-   print('Errors: [')
+   st.write(scope_tree)
+   st.write('Errors: [')
    for error in errors:
-       print('\t', error)
-   print(']')
-   print('Auto Types\n', auto_types)
-   print('Infered Types\n', infered_types)
-   
-run_pipeline(G, ejemplo9)
+       st.write('\t', error)
+   st.write(']')
+   st.write('Auto Types\n', auto_types)
+   for key in infered_types:
+      st.write('{\n\t' + '\n\t'.join(f'{key}: {infered_types[key]}' for key in infered_types))
 
+   # st.write('Infered Types\n', infered_types)
+
+if (run_analysis):
+   run_pipeline(G, data)
 

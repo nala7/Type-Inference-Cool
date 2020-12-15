07b7912d0c29d5c0a01ffed9dd290459a4d15911
ALabourdette <j.labourdette@estudiantes.matcom.uh.cu>
2020-12-15 15:41:37 -0500

    A

--- Main.py
+++ Main.py
@@ -44,41 +44,38 @@ st.sidebar.text("Grupo: C-312")
 
 """ # Cool Intrepreter """
 
-data = st.text_area("Enter code", "", 500)
-run_analysis = st.button("Run", "")
 
 def run_pipeline(G, text):
-   st.write('=================== TEXT ======================')
-   st.write(text)
-   st.write('================== TOKENS =====================')
+   # st.write('=================== TEXT ======================')
+   # st.write(text)
+   # st.write('================== TOKENS =====================')
    tokens = tokenize_text(text)
-   st.write('=================== PARSE =====================')
+   # st.write('=================== PARSE =====================')
    parser = Serializer.load(os.getcwd() + '/parser')
 
    parse, operations = parser([t.token_type for t in tokens], get_shift_reduce=True)
 
-   st.write('==================== AST ======================')
+   # st.write('==================== AST ======================')
    ast = evaluate_reverse_parse(parse, operations, tokens)
    formatter = print_ast.FormatVisitor()
    tree = formatter.visit(ast)
-   st.write(tree)
-   st.write('============== COLLECTING TYPES ===============')
+   # print (tree)
+   # st.text(tree)
+   # st.write('============== COLLECTING TYPES ===============')
    errors = []
    collector = TypeCollector(errors)
    collector.visit(ast)
    context = collector.context
-   st.write('Errors:', errors)
-   st.write('Context:')
-   st.write(context)
-   st.write('=============== BUILDING TYPES ================')
+   # st.write('Errors:', errors)
+   # st.write('Context:')
+   # st.text(str(context))
+   # st.write('=============== BUILDING TYPES ================')
    builder = TypeBuilder(context, errors)
    builder.visit(ast)
-   st.write('Errors: [')
-   for error in errors:
-      print('\t', error)
-   st.write(']')
-   st.write('Context:')
-   st.write(context)
+   # st.write('Errors:', errors)
+   # st.write('Context:',)
+   # st.text(str(context))
+   # st.write(context)
    st.write('=============== CHECKING TYPES ================')
    old_errors = errors.copy()
    checker = TypeChecker(context, old_errors)
@@ -94,17 +91,17 @@ def run_pipeline(G, text):
 
    st.write('Scope:')
    scope_tree = Scope_Print().visit(scope)
-   st.write(scope_tree)
-   st.write('Errors: [')
-   for error in errors:
-       st.write('\t', error)
-   st.write(']')
+   st.text(scope_tree)
+   st.write('Errors:', errors)
    st.write('Auto Types\n', auto_types)
-   for key in infered_types:
-      st.write('{\n\t' + '\n\t'.join(f'{key}: {infered_types[key]}' for key in infered_types))
+   st.write('Infered Types')
+   st.text('\n'.join(f'{key}: {infered_types[key].name}' for key in infered_types))
+   # st.write('{\n\t' + '\n\t'.join(f'{key}: {infered_types[key]}' for key in infered_types))
 
-   # st.write('Infered Types\n', infered_types)
+data = st.text_area("Enter code", "", 500)
+run_analysis = st.button("Run", "")
 
 if (run_analysis):
    run_pipeline(G, data)
+   # st.write('Infered Types\n', infered_types)
 
